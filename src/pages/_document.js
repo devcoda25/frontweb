d219eee6d51cd515a3b3523ed04e59bca770ff04
turyomwe. derrick <147058699+devcoda25@users.noreply.github.com>
2024-10-27 11:31:44 +0300

    Add files via upload

---
+++ src/pages/_document.js
@@ -0,0 +1,26 @@
+// ** React Import
+// ** Next Import
+import { Html, Head, Main, NextScript } from 'next/document'
+
+const CustomDocument = () => {
+  return (
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
+        <link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet"></link>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6247560589805667"
+     crossorigin="anonymous"></script>
+       
+     
+      </body>
+    </Html>
+  )
+}
+
+export default CustomDocument
